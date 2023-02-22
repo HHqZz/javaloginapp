b973fad13354f77287301e1b94e65b5b725923a9
HHqZz <cbouis@outlook.fr>
2023-02-22 16:33:20 -0500

    test

--- src/main/java/com/valaxy/demo/HomeController.java
+++ src/main/java/com/valaxy/demo/HomeController.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,27 +14,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class HomeController {
 
+  @RequestMapping("home")
+  public ModelAndView home() {
+    ModelAndView mv = new ModelAndView("home");
 
+    // mv.addObject("username", "");
 
-	@RequestMapping("home")
-	public ModelAndView home() {
-		ModelAndView mv = new ModelAndView("home");
+    return mv;
+  }
 
-		//mv.addObject("username", "");
+  @RequestMapping("/")
+  public ModelAndView Default() {
+    ModelAndView mv = new ModelAndView("home");
 
-		return mv;
-	}
+    // mv.addObject("username", "");
 
-	@RequestMapping("/")
-	public ModelAndView Default() {
-		ModelAndView mv = new ModelAndView("home");
-
-		//mv.addObject("username", "");
-
-		return mv;
-	}
-
-
-
-
+    return mv;
+  }
 }
